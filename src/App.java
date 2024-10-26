3b7a15cd728503dcfe7102635cf4293b31e844a8
Fernando Moro <Morofernando99@gmail.com>
2024-10-26 12:26:26 -0300

    changed variables names in main

--- src/App.java
+++ src/App.java
@@ -1,43 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
-      // RODADA 01
-      int SEED_RODADA_01 = 1;
-      NumbersArray RODADA_01_1000 = NumbersArray.generate(1000, SEED_RODADA_01);
-      NumbersArray RODADA_01_10000 = NumbersArray.generate(10000, SEED_RODADA_01);
-      NumbersArray RODADA_01_100000 = NumbersArray.generate(100000, SEED_RODADA_01);
-      NumbersArray RODADA_01_500000 = NumbersArray.generate(500000, SEED_RODADA_01);
-      NumbersArray RODADA_01_1000000 = NumbersArray.generate(1000000, SEED_RODADA_01);
+      // RODADA 01 (R1)
+      int SEED_R1 = 1;
+      NumbersArray R1_1000 = NumbersArray.generate(1000, SEED_R1);
+      NumbersArray R1_10000 = NumbersArray.generate(10000, SEED_R1);
+      NumbersArray R1_100000 = NumbersArray.generate(100000, SEED_R1);
+      NumbersArray R1_500000 = NumbersArray.generate(500000, SEED_R1);
+      NumbersArray R1_1000000 = NumbersArray.generate(1000000, SEED_R1);
 
-      // RODADA 02
-      int SEED_RODADA_02 = 1;
-      NumbersArray RODADA_02_1000 = NumbersArray.generate(1000, SEED_RODADA_02);
-      NumbersArray RODADA_02_10000 = NumbersArray.generate(10000, SEED_RODADA_02);
-      NumbersArray RODADA_02_100000 = NumbersArray.generate(100000, SEED_RODADA_02);
-      NumbersArray RODADA_02_500000 = NumbersArray.generate(500000, SEED_RODADA_02);
-      NumbersArray RODADA_02_1000000 = NumbersArray.generate(1000000, SEED_RODADA_02);
+      // RODADA 02 (R2)
+      int SEED_R2 = 1;
+      NumbersArray R2_1000 = NumbersArray.generate(1000, SEED_R2);
+      NumbersArray R2_10000 = NumbersArray.generate(10000, SEED_R2);
+      NumbersArray R2_100000 = NumbersArray.generate(100000, SEED_R2);
+      NumbersArray R2_500000 = NumbersArray.generate(500000, SEED_R2);
+      NumbersArray R2_1000000 = NumbersArray.generate(1000000, SEED_R2);
 
-      // RODADA 03
-      int SEED_RODADA_03 = 1;
-      NumbersArray RODADA_03_1000 = NumbersArray.generate(1000, SEED_RODADA_03);
-      NumbersArray RODADA_03_10000 = NumbersArray.generate(10000, SEED_RODADA_03);
-      NumbersArray RODADA_03_100000 = NumbersArray.generate(100000, SEED_RODADA_03);
-      NumbersArray RODADA_03_500000 = NumbersArray.generate(500000, SEED_RODADA_03);
-      NumbersArray RODADA_03_1000000 = NumbersArray.generate(1000000, SEED_RODADA_03);
+      // RODADA 03 (R3)
+      int SEED_R3 = 1;
+      NumbersArray R3_1000 = NumbersArray.generate(1000, SEED_R3);
+      NumbersArray R3_10000 = NumbersArray.generate(10000, SEED_R3);
+      NumbersArray R3_100000 = NumbersArray.generate(100000, SEED_R3);
+      NumbersArray R3_500000 = NumbersArray.generate(500000, SEED_R3);
+      NumbersArray R3_1000000 = NumbersArray.generate(1000000, SEED_R3);
 
-      // RODADA_04
-      int SEED_RODADA_04 = 1;
-      NumbersArray RODADA_04_1000 = NumbersArray.generate(1000, SEED_RODADA_04);
-      NumbersArray RODADA_04_10000 = NumbersArray.generate(10000, SEED_RODADA_04);
-      NumbersArray RODADA_04_100000 = NumbersArray.generate(100000, SEED_RODADA_04);
-      NumbersArray RODADA_04_500000 = NumbersArray.generate(500000, SEED_RODADA_04);
-      NumbersArray RODADA_04_1000000 = NumbersArray.generate(1000000, SEED_RODADA_04);
+      // R4 (R4)
+      int SEED_R4 = 1;
+      NumbersArray R4_1000 = NumbersArray.generate(1000, SEED_R4);
+      NumbersArray R4_10000 = NumbersArray.generate(10000, SEED_R4);
+      NumbersArray R4_100000 = NumbersArray.generate(100000, SEED_R4);
+      NumbersArray R4_500000 = NumbersArray.generate(500000, SEED_R4);
+      NumbersArray R4_1000000 = NumbersArray.generate(1000000, SEED_R4);
 
-      // RODADA 05
-      int SEED_RODADA_05 = 1;
-      NumbersArray RODADA_05_1000 = NumbersArray.generate(1000, SEED_RODADA_05);
-      NumbersArray RODADA_05_10000 = NumbersArray.generate(10000, SEED_RODADA_05);
-      NumbersArray RODADA_05_100000 = NumbersArray.generate(100000, SEED_RODADA_05);
-      NumbersArray RODADA_05_500000 = NumbersArray.generate(500000, SEED_RODADA_05);
-      NumbersArray RODADA_05_1000000 = NumbersArray.generate(1000000, SEED_RODADA_05);
+      // RODADA 05 (R5)
+      int SEED_R5 = 1;
+      NumbersArray R5_1000 = NumbersArray.generate(1000, SEED_R5);
+      NumbersArray R5_10000 = NumbersArray.generate(10000, SEED_R5);
+      NumbersArray R5_100000 = NumbersArray.generate(100000, SEED_R5);
+      NumbersArray R5_500000 = NumbersArray.generate(500000, SEED_R5);
+      NumbersArray R5_1000000 = NumbersArray.generate(1000000, SEED_R5);
     }
 }
